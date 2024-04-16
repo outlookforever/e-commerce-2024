d16c762f7363b7bf1353f0e63099c15693c6b290
Sergey Kochetov <anashkagonit@gmail.com>
2024-04-16 19:12:42 +0300

    end Header components UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa'
+import { FaRegCircleUser } from 'react-icons/fa6'
 import { GrSearch } from 'react-icons/gr'
 
 import Logo from './Logo'
@@ -23,7 +25,24 @@ const Header = () => {
 						<GrSearch />
 					</div>
 				</div>
-				<div className=''>user icons</div>
+				<div className='flex items-center gap-7'>
+					<div className='text-3xl cursor'>
+						<FaRegCircleUser />
+					</div>
+					<div className='text-2xl cursor relative'>
+						<span>
+							<FaShoppingCart />
+						</span>
+						<div className='text-white bg-red-600 w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
+							<p className='text-xs'>0</p>
+						</div>
+					</div>
+					<div className=''>
+						<button className='px-3 py-1 bg-red-600 rounded-full text-white hover:bg-red-700'>
+							Login
+						</button>
+					</div>
+				</div>
 			</div>
 		</header>
 	)
